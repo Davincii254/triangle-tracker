dc25380971ed2365ff0fe7da1ead3ba28586c663
victor kuria <victorkuria7@gmail.com>
2020-07-31 10:41:03 +0300

    will change code variable

--- js/script.js
+++ js/script.js
@@ -9,7 +9,7 @@ var triangle = function(sideOne, sideTwo, sideThree){
   
   
   var isTriangle = function(sideOne, sideTwo, sideThree) {
-    return sideOne <= (sideTwo + sideThree) && sideTwo <= (sideThree + sideOne) && sideThree <= (sideOne + sideTwo)
+    return sideOne => (sideTwo + sideThree) && sideTwo <= (sideThree + sideOne) && sideThree <= (sideOne + sideTwo)
   };
 
 
